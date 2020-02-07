4cbe0d145ccd203c3d15fab03a79e0676a3c46cc
Kyle Richardson <52683176+kyle-richardson@users.noreply.github.com>
2020-02-06 22:56:44 -0600

    Revert "seeding and migration done, server crashes"

--- data/seeds/01-recipes.js
+++
@@ -1,8 +0,0 @@
-exports.seed = function(knex) {
-    return knex('recipes').insert([
-      {recipe_name: 'Cupcakes'},
-      {recipe_name: 'Cake'},
-      {recipe_name: 'Brownies'},
-    ]);
-  };
-  
